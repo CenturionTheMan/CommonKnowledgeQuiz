792e31b37bb5264326211e0549631f6b011c97e9
CenturionTheMan <dziedziakmichal0@gmail.com>
2024-10-19 20:38:46 +0200

    two screens

--- CKQKotlin/app/src/main/java/com/example/ckqkotlin/MainActivity.kt
+++ CKQKotlin/app/src/main/java/com/example/ckqkotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.ckqkotlin
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton : Button
+    private lateinit var timePerQuestionEditText : EditText
+    private lateinit var questionAmountEditText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        startButton = findViewById<Button>(R.id.startButton)
+        timePerQuestionEditText = findViewById(R.id.timePerQuestionEditText)
+        questionAmountEditText = findViewById(R.id.questionAmountEditText)
+
+        startButton.setOnClickListener({v: View ->
+            setContentView(R.layout.activity_in_game)
+        })
     }
 }
